431fe3596f1052ba4a9a22055c837652efb80f08
mathucv24 <barriosrudas@gmail.com>
2026-08-07 21:28:33 -0400

    Mail service configuration has been updated to use the 'host', 'port', and 'secure' options instead of the 'service' option. This change is necessary for compatibility with certain email providers and to ensure secure email transmission.

--- controllers/sesionController.js
+++ controllers/sesionController.js
@@ -5,11 +5,12 @@ import Pago from '../models/pago.js';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS 
-        
     }
 });
 
